b960a316cea933a0e5f9f178ede24ee3a6f2b5a7
Michael DeRoy <michaelde@unity3d.com>
2017-12-06 13:37:39 -0500

    Remove Il2cppMonoString

--- mono/mini/il2cpp-c-types.h
+++ mono/mini/il2cpp-c-types.h
@@ -21,6 +21,7 @@
 #define MonoGenericClass Il2CppGenericClass
 #define MonoGenericContainer Il2CppGenericContainer
 #define MonoProperty Il2CppPropertyInfo
+#define MonoString Il2CppString
 
 //still stubs everywhere
 typedef struct _Il2CppMonoAssemblyName Il2CppMonoAssemblyNameReplacement;
@@ -29,7 +30,6 @@ typedef struct _Il2CppMonoDomain Il2CppMonoDomain;
 typedef struct _Il2CppMonoMethodSignature Il2CppMonoMethodSignature;
 typedef struct _Il2CppMonoMethodHeader Il2CppMonoMethodHeader;
 typedef struct _Il2CppMonoVTable Il2CppMonoVTable;
-typedef struct Il2CppString Il2CppMonoString;
 typedef struct _Il2CppMonoAppDomain Il2CppMonoAppDomain;
 typedef struct _Il2CppMonoMarshalByRefObject Il2CppMonoMarshalByRefObject;
 typedef struct _Il2CppMonoObject Il2CppMonoObject;
